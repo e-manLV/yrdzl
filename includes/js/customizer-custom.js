11b579a4ae5ba2911951e6b97b61670edd9a6145
Allison Lubsey <allison_lubsey@hotmail.com>
2021-01-28 17:50:43 -0800

    Delete customizer-custom.js

--- includes/js/customizer-custom.js
+++
@@ -1,8 +0,0 @@
-/**
- * Remove select values that don't work with this Overlay Child
- */
-( function( $ ) {
-    jQuery( document ).ready( function() {
-        $( "#customize-control-overlay-stick-header-element select option[value='stick-onlynav']" ).remove();
-    });
-} )( jQuery );
